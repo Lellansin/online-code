50ee73e7fb74429703d41a871c3da6a1b4323653
Lellansin <lellansin@gmail.com>
2019-07-06 15:47:59 +0800

    feat: 本地 upload pages

--- build.sh
+++ build.sh
@@ -1,9 +1,11 @@
 ROOT=$PWD/node_modules/monaco-editor/esm/vs
 OPTS="--no-source-maps --log-level 1"        # Parcel options - See: https://parceljs.org/cli.html
 
+rm -rf dist/
 parcel build $ROOT/language/json/json.worker.js $OPTS
 parcel build $ROOT/language/css/css.worker.js $OPTS
 parcel build $ROOT/language/html/html.worker.js $OPTS
 parcel build $ROOT/language/typescript/ts.worker.js $OPTS
 parcel build $ROOT/editor/editor.worker.js $OPTS
+parcel build index.html $OPTS
 
